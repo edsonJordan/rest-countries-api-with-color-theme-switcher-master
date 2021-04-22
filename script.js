7136cdecf3dce3a2b5478a7c3f2ac5803ef337d7
Edson Jordan <edson_4555@hotmail.com>
2021-04-22 00:07:47 -0500

    div: debe tener un id no un value

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ let painCards = (dataReg)=>{
    let contCard= document.getElementById('content-cards');
    contCard.innerHTML= '';
    for (const key in dataReg) {
-      contCard.innerHTML+=`<div class="card" value="${dataReg[key].alpha3Code}">
+      contCard.innerHTML+=`<div class="card" id="${dataReg[key].alpha3Code}">
       <img src="${dataReg[key].flag}" class="img img__card" alt="">
       <label for="${key}" class="label label__country">${dataReg[key].name}</label>
       <ul>
@@ -67,7 +67,7 @@ document.getElementById('input__content').addEventListener('keyup', (e)=>{
             let result = new String(iterator.name.toLowerCase().trim().replace(/ /g, ""));
             if(result.indexOf(keySearch) !== -1){
                console.log(iterator.alpha3Code);
-               contCard.innerHTML+=`<div class="card" value="${iterator.alpha3Code}">
+               contCard.innerHTML+=`<div class="card" id="${iterator.alpha3Code}">
                <img src="${iterator.flag}" class="img img__card" alt="">
                <label for="${iterator.alpha3Code}" class="label label__country">${iterator.name}</label>
                <ul>
