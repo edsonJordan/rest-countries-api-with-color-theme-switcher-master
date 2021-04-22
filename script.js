fd8385eafa3d70301b4f1ba97f02c21ff125a8d7
Edson Jordan <edson_4555@hotmail.com>
2021-04-21 23:52:48 -0500

    for:debe tener un codigo

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ let painCards = (dataReg)=>{
    for (const key in dataReg) {
       contCard.innerHTML+=`<div class="card" id="${dataReg[key].alpha3Code}">
       <img src="${dataReg[key].flag}" class="img img__card" alt="">
-      <label for="${dataReg[key]}" class="label label__country">${dataReg[key].name}</label>
+      <label for="${key}" class="label label__country">${dataReg[key].name}</label>
       <ul>
       <li class="li data__li" attr-data="${dataReg[key].population}">Population: </li>
       <li class="li data__li" attr-data="${dataReg[key].region}">Region: </li>
@@ -66,6 +66,7 @@ document.getElementById('input__content').addEventListener('keyup', (e)=>{
             } 
             let result = new String(iterator.name.toLowerCase().trim().replace(/ /g, ""));
             if(result.indexOf(keySearch) !== -1){
+               console.log(iterator.alpha3Code);
                contCard.innerHTML+=`<div class="card" id="${iterator.alpha3Code}">
                <img src="${iterator.flag}" class="img img__card" alt="">
                <label for="${iterator.alpha3Code}" class="label label__country">${iterator.name}</label>
